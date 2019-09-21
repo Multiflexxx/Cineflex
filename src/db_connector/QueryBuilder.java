1c19c29357645dded5b42b9526c78bf9c97dd301
NerdyStuff <NerdyStuff@users.noreply.github.com>
2019-09-21 22:03:31 +0200

    Added Queries

--- src/db_connector/QueryBuilder.java
+++ src/db_connector/QueryBuilder.java
@@ -1,5 +1,8 @@
 package db_connector;
 
+import java.util.*;
+import java.text.*;
+
 public class QueryBuilder {
     public static String createLoginQuery(String email, String passwordHash)
     {
@@ -9,5 +12,21 @@ public class QueryBuilder {
     public static String createUser(String name, String lastname, String gebDate, String email, String passwordHash)
     {
         return "INSERT INTO Person (`Vorname`, `Nachname`, `GebDatum`, `E-Mail`, `Passwort`) VALUES ('"+name+"', '"+lastname+"', '"+gebDate+"', '"+email+"', '"+passwordHash+"'); \n INSERT INTO Kunde (`PID`, `Treuepunkte`) VALUES ((SELECT `PID` FROM Person WHERE `Vorname` = '"+name+"' AND `Nachname` = '"+lastname+"' AND `GebDatum` = '"+gebDate+"' AND `E-Mail` = '"+email+"' AND `Passwort` = '"+passwordHash+"'), 0);";
+    }
+
+    public static String showCinemaImaginationsToday()
+    {
+        return "SELECT * FROM Vorstellung WHERE `Datum` = '"+ getDateAsString() +"'; ";
+    }
+
+    public static String showCinemaImaginationsThisWeek()
+    {
+        return "SELECT * FROM Vorstellung WHERE `Datum` >= '"+ getDateAsString() +"'; ";
+    }
+
+    private static String getDateAsString()
+    {
+        DateFormat formatter = new SimpleDateFormat("yyy-MM-dd");
+        return formatter.format(new Date());
     }
 }
