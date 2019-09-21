361731a51ae9f3a67d0996be38bdebfa91d59639
NerdyStuff <NerdyStuff@users.noreply.github.com>
2019-09-21 19:48:05 +0200

    Update QueryBuilder

--- src/db_connector/QueryBuilder.java
+++ src/db_connector/QueryBuilder.java
@@ -1,14 +1,12 @@
 package db_connector;
 
-import java.util.Date;
-
 public class QueryBuilder {
     public static String createLoginQuery(String username, String passwordHash)
     {
         return "SELECT * FROM Person WHERE `Vorname` = '" + username + "' AND `Passwort` = '" + passwordHash + "' ;";
     }
 
-    public static String createUser(String name, String lastname, Date gebDate, String email, String passwordHash)
+    public static String createUser(String name, String lastname, String gebDate, String email, String passwordHash)
     {
         return "INSERT INTO Person (`Vorname`, `Nachname`, `GebDatum`, `E-Mail`, `Passwort`) VALUES ('"+name+"', '"+lastname+"', '"+gebDate+"', '"+email+"', '"+passwordHash+"'); INSERT INTO Kunde (`PID`, `Treuepunkte`) VALUES (SELECT `PID` FROM Person WHERE `Vorname` = '"+name+"' AND `Nachname` = '"+lastname+"' AND `GebDatum` = '"+gebDate+"' AND `E-Mail` = '"+email+"' AND `Passwort` = '"+passwordHash+"', 0)";
     }
