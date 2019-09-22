efde75cdbd8ca95c05590d24ab73a36fcced99e2
NerdyStuff <NerdyStuff@users.noreply.github.com>
2019-09-22 23:16:23 +0200

    Update javascript

--- web/javascript/cookie_url.js
+++ web/javascript/cookie_url.js
@@ -2,8 +2,8 @@ function setCookieUrl(city, plz) {
     var d = new Date();
     d.setTime(d.getTime() + (60*60*24*365));
     var expires = "expires="+ d.toUTCString();
-    document.cookie = "city=" + city + expires + ";path=/";
-    document.cookie = "plz=" + plz + expires + ";path=/";
+    document.cookie = "city=" + city + ";" + expires + ";path=/";
+    document.cookie = "plz=" + plz + ";" + expires + ";path=/";
     history.pushState(null, '', (window.location.pathname + "?city=" + city));
     document.getElementById("navbarDropdownCity").innerHTML = city;
 }
