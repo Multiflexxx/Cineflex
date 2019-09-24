1cd6c3b8c0666d505c7318a0d5e9c9395127239d
Anton <antonochel@web.de>
2019-09-24 21:11:34 +0200

    film hinzugefügt

--- src/oo/Film.java
+++ src/oo/Film.java
@@ -1,7 +1,82 @@
 package oo;
 
 public class Film {
-    String title;
-    String description;
+    String titel, beschreibung, bildLink, trailerLink;
+    int dauer, fsk, filmID;
+    boolean dreiD;
 
+    public Film(String titel, String beschreibung, String bildLink, String trailerLink, int dauer, int fsk, int filmID, boolean dreiD) {
+        this.titel = titel;
+        this.beschreibung = beschreibung;
+        this.bildLink = bildLink;
+        this.trailerLink = trailerLink;
+        this.dauer = dauer;
+        this.fsk = fsk;
+        this.filmID = filmID;
+        this.dreiD = dreiD;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public String getBildLink() {
+        return bildLink;
+    }
+
+    public void setBildLink(String bildLink) {
+        this.bildLink = bildLink;
+    }
+
+    public String getTrailerLink() {
+        return trailerLink;
+    }
+
+    public void setTrailerLink(String trailerLink) {
+        this.trailerLink = trailerLink;
+    }
+
+    public int getDauer() {
+        return dauer;
+    }
+
+    public void setDauer(int dauer) {
+        this.dauer = dauer;
+    }
+
+    public int getFsk() {
+        return fsk;
+    }
+
+    public void setFsk(int fsk) {
+        this.fsk = fsk;
+    }
+
+    public int getFilmID() {
+        return filmID;
+    }
+
+    public void setFilmID(int filmID) {
+        this.filmID = filmID;
+    }
+
+    public boolean isDreiD() {
+        return dreiD;
+    }
+
+    public void setDreiD(boolean dreiD) {
+        this.dreiD = dreiD;
+    }
 }
