2195b4a590326136efcf175c89c5e5fcb6e6db29
TristanMrb <trisemig@aol.com>
2019-10-08 14:46:40 +0200

    Update des SingleMovie

--- web/javascript/filter.js
+++ web/javascript/filter.js
@@ -1,8 +1,8 @@
-window.onload=function() {
-    var d = new Date();
-    var date = d.getFullYear() + "-" + ("0" + d.getMonth()).slice(-2)+ "-" + ("0" + d.getDate()).slice(-2);
-    document.getElementById("inputDate").value = date;
-}
+// window.onload=function() {
+//     var d = new Date();
+//     var date = d.getFullYear() + "-" + ("0" + d.getMonth()).slice(-2)+ "-" + ("0" + d.getDate()).slice(-2);
+//     document.getElementById("inputDate").value = date;
+// }
 
 function submitFilter() {
     document.SearchForm.submit();
