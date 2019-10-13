195e39b4f20133706aff0c4a3891c8bb5c8c67c9
NerdyStuff <NerdyStuff@users.noreply.github.com>
2019-10-13 17:38:59 +0200

    Fixed Seats Style -> needs Color name for if!

--- web/javascript/seats.js
+++ web/javascript/seats.js
@@ -1,7 +1,15 @@
 function chooseSeat(id) {
-    if (document.getElementById(id).style.backgroundColor === '#42f557') {
+
+    if(document.getElementById(id).style.backgroundColor == "#ff0000")
+    {
+        return;
+    }
+
+    console.log(document.getElementById(id).style.backgroundColor);
+
+    if (document.getElementById(id).style.backgroundColor == "green") {
         document.getElementById(id).style.backgroundColor = '#4a9be8';
     } else {
-        document.getElementById(id).style.backgroundColor = '#42f557';
+        document.getElementById(id).style.backgroundColor = "green";
     }
 }
