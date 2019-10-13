7138488e6e48e779bc747fa479a3a87feca8b42d
Anton <antonochel@web.de>
2019-10-13 16:55:22 +0200

    seats javascript

--- web/javascript/seats.js
+++ web/javascript/seats.js
@@ -1,7 +1,7 @@
-function chooseSeat() {
-    if (this.style.backgroundColor === '#42f557') {
-        this.style.backgroundColor = '#4a9be8';
+function chooseSeat(id) {
+    if (document.getElementById(id).style.backgroundColor === '#42f557') {
+        document.getElementById(id).style.backgroundColor = '#4a9be8';
     } else {
-        this.style.backgroundColor = '#42f557';
+        document.getElementById(id).style.backgroundColor = '#42f557';
     }
 }
