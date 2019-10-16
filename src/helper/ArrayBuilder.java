0eff418ad96f8e434d8673309efbdfbda1d28fe2
Grimmig18 <benno.grimm@gmx.de>
2019-10-16 20:13:07 +0200

    Work on buchungs process

---
+++ src/helper/ArrayBuilder.java
@@ -0,0 +1,29 @@
+package helper;
+
+public class ArrayBuilder {
+    public static int[] stringToIntArray(String string, String del) {
+        String[] splitString = string.split(del);
+        int[] arr = new int[splitString.length];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(splitString[i]);
+        }
+
+        return arr;
+    }
+
+    public static String intArrayToString(int[] arr) {
+        return intArrayToString(arr, ", ");
+    }
+
+    public static String intArrayToString(int[] arr, String del) {
+        String output = "";
+        for(int i = 0; i< arr.length; i++) {
+            if(i != arr.length - 1) {
+                output += i + ", ";
+            } else {
+                output += i;
+            }
+        }
+        return output;
+    }
+}
