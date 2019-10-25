2f4977a9e0303025b4e9833a9b5dcec85541ad64
TristanMrb <trisemig@aol.com>
2019-10-25 23:42:11 +0200

    Add Pdf and QR-Code Generator

---
+++ src/qr_code/QrCodeGenerator.java
@@ -0,0 +1,35 @@
+package qr_code;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+//https://www.callicoder.com/generate-qr-code-in-java-using-zxing/
+public class QrCodeGenerator {
+    private static final String QR_CODE_IMAGE_PATH = "web/img/qrcode/MyQRCode.png";
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    public static void main(String[] args) {
+        try {
+            generateQRCodeImage("KID: 69, Bestellung: 420", 480, 480, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
