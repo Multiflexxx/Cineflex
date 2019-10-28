a1473f23c0b40f3a99d7f72beab69bbae5eb0ca1
TristanMrb <trisemig@aol.com>
2019-10-28 22:43:01 +0100

    Add QrCode encrypting

--- src/main/java/qr_code/QrCodeGenerator.java
+++ src/main/java/qr_code/QrCodeGenerator.java
@@ -7,19 +7,17 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.io.IOException;
-import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 //https://www.callicoder.com/generate-qr-code-in-java-using-zxing/
 public class QrCodeGenerator {
-    private static final String QR_CODE_IMAGE_PATH = "../../../GitProjekte/CineflexV1/web/img/qrcode/MyQRCode.png;";  //"web/img/qrcode/MyQRCode.png";
+    private static final String QR_CODE_IMAGE_PATH = "../../../GitProjekte/Cineflex/web/img/qrcode/MyQRCode.png;";  //"web/img/qrcode/MyQRCode.png";
 
     public static void generateQRCodeImage(String text, String filepath)
             throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 300, 300);
+        BitMatrix bitMatrix = qrCodeWriter.encode(EncryptQr.encrypt(text, "9S25TB8TBP7PWW6T2XGQ6BM8DZBLYACS"), BarcodeFormat.QR_CODE, 300, 300);
 
         //Path path = FileSystems.getDefault().getPath(filepath);
         Path path = FileSystems.getDefault().getPath(filepath);
@@ -29,7 +27,7 @@ public class QrCodeGenerator {
 
     public static void main(String[] args) {
         try {
-            generateQRCodeImage("KID: 691111", QR_CODE_IMAGE_PATH);
+            generateQRCodeImage("Test", "web/img/qrcode/MyQRCode.png");
         } catch (WriterException e) {
             System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
         } catch (IOException e) {
