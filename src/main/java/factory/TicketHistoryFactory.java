9372a882b543d5dba7ef95656a3063a267b24ba7
Grimmig18 <benno.grimm@gmx.de>
2019-10-29 09:51:06 +0100

    Fix in TicketHistoryfactory

--- src/main/java/factory/TicketHistoryFactory.java
+++ src/main/java/factory/TicketHistoryFactory.java
@@ -21,7 +21,7 @@ public class TicketHistoryFactory {
 				buchungsbelege = BuchungsFactory.getBuchungsbelegeByKID(KID);
 				reservierungsbelege = ReservierungsFactory.getReservierungsbelegByKID(KID);
 
-				int length = buchungsbelege.length + reservierungsbelege.length;
+				int length = (buchungsbelege == null ? 0 : buchungsbelege.length) + (reservierungsbelege == null ? 0 : reservierungsbelege.length);
 
 				if(length < 1) {
 						return null;
@@ -30,51 +30,59 @@ public class TicketHistoryFactory {
 				ticketHistories = new TicketHistory[length];
 
 				// Add Buchungen to TicketHistory
-				for(int i = 0; i < buchungsbelege.length; i++) {
-						// Get Stornierung (if there is one)
-						BuchungsStornierung buchungsStornierung = null;
-						try {
-								buchungsStornierung = BuchungsStornierungFactory.getBuchungsStornierungByBNR(buchungsbelege[i].getBelegID());
-						} catch (ResultSetIsNullException | FailedObjectCreationException e) {
-								e.printStackTrace();
-								throw new RequiredFactoryFailedException();
-						} catch (EmptyResultSetException e) {
-								// Buchung not cancelled
+				if(buchungsbelege != null) {
+						for (int i = 0; i < buchungsbelege.length; i++) {
+								// Get Stornierung (if there is one)
+								BuchungsStornierung buchungsStornierung = null;
+								try {
+										buchungsStornierung = BuchungsStornierungFactory
+												.getBuchungsStornierungByBNR(buchungsbelege[i].getBelegID());
+								} catch (ResultSetIsNullException | FailedObjectCreationException e) {
+										e.printStackTrace();
+										throw new RequiredFactoryFailedException();
+								} catch (EmptyResultSetException e) {
+										// Buchung not cancelled
+								}
+
+								// Get seats for Buchung
+								Sitz[] sitze = null;
+								sitze = SitzFactory.getSitzeByBNR(buchungsbelege[i].getBelegID());
+
+								if (sitze == null) {
+										throw new RequiredFactoryFailedException();
+								}
+
+								ticketHistories[i] = new TicketHistory(buchungsbelege[i], sitze,
+										buchungsStornierung);
 						}
-
-						// Get seats for Buchung
-						Sitz[] sitze = null;
-						sitze = SitzFactory.getSitzeByBNR(buchungsbelege[i].getBelegID());
-
-						if(sitze == null) {
-								throw new RequiredFactoryFailedException();
-						}
-
-						ticketHistories[i] = new TicketHistory(buchungsbelege[i], sitze, buchungsStornierung);
 				}
 
 				// Add Reservierungen to ticketHistory
-				for(int i = 0; i < reservierungsbelege.length; i++) {
-						// Get Stornierung for Reservierung (if there is one)
-						ReservierungsStornierung reservierungsStornierung = null;
-						try {
-								reservierungsStornierung = ReservierungsStornierungFactory.getReservierungsStornierungByRNR(reservierungsbelege[i].getBelegID());
-						} catch (ResultSetIsNullException | FailedObjectCreationException e) {
-								e.printStackTrace();
-								throw new RequiredFactoryFailedException();
-						} catch (EmptyResultSetException e) {
-								// Reservierung Cancelled
+				if(reservierungsbelege != null) {
+						for (int i = 0; i < reservierungsbelege.length; i++) {
+								// Get Stornierung for Reservierung (if there is one)
+								ReservierungsStornierung reservierungsStornierung = null;
+								try {
+										reservierungsStornierung = ReservierungsStornierungFactory
+												.getReservierungsStornierungByRNR(reservierungsbelege[i].getBelegID());
+								} catch (ResultSetIsNullException | FailedObjectCreationException e) {
+										e.printStackTrace();
+										throw new RequiredFactoryFailedException();
+								} catch (EmptyResultSetException e) {
+										// Reservierung Cancelled
+								}
+
+								// Get Seats for Reservierung
+								Sitz[] sitze = null;
+								sitze = SitzFactory.getSitzeByRNR(reservierungsbelege[i].getBelegID());
+
+								if (sitze == null) {
+										throw new RequiredFactoryFailedException();
+								}
+
+								ticketHistories[buchungsbelege.length + i] = new TicketHistory(
+										reservierungsbelege[i], sitze, reservierungsStornierung);
 						}
-
-						// Get Seats for Reservierung
-						Sitz[] sitze = null;
-						sitze = SitzFactory.getSitzeByRNR(reservierungsbelege[i].getBelegID());
-
-						if(sitze == null) {
-								throw new RequiredFactoryFailedException();
-						}
-
-						ticketHistories[buchungsbelege.length + i] = new TicketHistory(reservierungsbelege[i], sitze, reservierungsStornierung);
 				}
 
 				return ticketHistories;
